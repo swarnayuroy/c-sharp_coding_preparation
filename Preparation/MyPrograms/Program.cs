8390032bbfb58a974a06e31107d089e10b83310f
DESKTOP-6NRNP7L\HP <swarnayuroy@gmail.com>
2026-07-18 14:49:14 +0530

    writing miscellaneous program

--- Preparation/MyPrograms/Program.cs
+++ Preparation/MyPrograms/Program.cs
@@ -11,38 +11,148 @@ namespace MyPrograms
     {
         static void Main(string[] args)
         {
-            while (true) {
+            while (true)
+            {
                 Console.Clear();
+                Console.WriteLine("Select any choice of your program to execute:\n1. String Programs\n2. Miscellaneous Programs");
+                Console.Write("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
 
-                #region String Program
-                Console.Write("Enter a word or a statement: ");
-                string userInput = Console.ReadLine();
-
-                if (string.IsNullOrEmpty(userInput)) 
+                try
                 {
-                    Console.WriteLine("Please enter any word or statement");
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Clear();
+                            #region String Programs
+
+                            Console.Write("Enter a word or a statement: ");
+                            string userInput = Console.ReadLine();
+
+                            if (string.IsNullOrEmpty(userInput))
+                            {
+                                Console.WriteLine("Please enter any word or statement");
+                                Console.ReadKey();
+
+                                break;
+                            }
+
+                            IStringProgram stringProgram = new StringProgram(userInput);
+                            stringProgram.Print_First_Non_Repeating_Character().Wait();
+                            stringProgram.Print_Lengthy_Word_from_given_Statement().Wait();
+                            stringProgram.Print_Vowels_With_Count().Wait();
+                            stringProgram.Print_Reverse_Words().Wait();
+                            stringProgram.Print_Letters_Count().Wait();
+                            #endregion
+
+                            Console.ReadKey();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            #region Miscellaneous Programs
+
+                            IMiscellaneousProgram miscellaneousProgram = new MiscellaneousProgram();
+
+                            #region Printing the array elements after right shifting them by a given number of times
+
+                            Console.Write("Enter a set of numbers separated by commas: ");
+                            string userInput4 = Console.ReadLine();
+                            if (string.IsNullOrEmpty(userInput4))
+                            {
+                                throw new Exception("Couldn't proceed with miscellaneous operations further with empty inputs!");
+                            }
+                            int[] numbersInput4 = userInput4.Split(',').Select(int.Parse).ToArray();
+                            Console.Write("Enter the number of times to right shift: ");
+                            int times = int.Parse(Console.ReadLine());
+                            miscellaneousProgram.Right_Shift_and_Print_Array_elements(numbersInput4, times).Wait();
+
+                            #endregion
+
+                            #region Printing the array elements after removing duplicates
+
+                            Console.Write("Enter a set of numbers separated by commas: ");
+                            string userInput3 = Console.ReadLine();
+                            if (string.IsNullOrEmpty(userInput3))
+                            {
+                                throw new Exception("Couldn't proceed with miscellaneous operations further with empty inputs!");
+                            }
+                            int[] numbersInput3 = userInput3.Split(',').Select(int.Parse).ToArray();
+                            miscellaneousProgram.Remove_Duplicate_elements_in_an_Array(numbersInput3).Wait();
+
+                            #endregion
+
+                            #region Printing max negative number from the given set of numbers
+
+                            Console.Write("Enter a set of numbers separated by commas: ");
+                            string userInput1 = Console.ReadLine();
+                            if (string.IsNullOrEmpty(userInput1))
+                            {
+                                throw new Exception("Couldn't proceed with miscellaneous operations further with empty inputs!");
+                            }
+                            double[] numbersInput1 = userInput1.Split(',').Select(double.Parse).ToArray();
+                            miscellaneousProgram.Print_Max_Negated_Number(numbersInput1).Wait();
+                            Console.ReadKey();
+
+                            #endregion
+
+                            #region Printing Nth largest number from the given set of numbers
+
+                            Console.Write("Enter a set of numbers separated by commas: ");
+                            string userInput2 = Console.ReadLine();
+                            if (string.IsNullOrEmpty(userInput2))
+                            {
+                                throw new Exception("Couldn't proceed with miscellaneous operations further with empty inputs!");
+                            }
+                            double[] numbersInput2 = userInput2.Split(',').Select(double.Parse).ToArray();
+
+                            Console.Write("Enter the value of index: ");
+                            int n_th_Index = int.Parse(Console.ReadLine());
+                            if ((n_th_Index >= 0) && (n_th_Index <= numbersInput2.Length))
+                            {
+                                miscellaneousProgram.Print_Nth_Largest_Number(numbersInput2, n_th_Index).Wait();
+                            }
+                            else
+                            {
+                                throw new Exception("Couldn't proceed with miscellaneous operations with invalid index!");
+                            }
+
+                            #endregion
+
+                            #region Printing sum of all digits of a number until it's result is a single digit
+
+                            Console.Write("Enter a number: ");
+                            int numberInput = int.Parse(Console.ReadLine());
+                            if (numberInput <= 0)
+                            {
+                                throw new Exception("Couldn't proceed with miscellaneous operations with invalid number input!");
+                            }
+                            miscellaneousProgram.Print_till_Sums_up_single_Digit(numberInput).Wait();
+
+                            #endregion
+
+                            #endregion
+
+                            Console.ReadKey();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
                     Console.ReadKey();
-                    
-                    continue;
                 }
                 
-                IStringProgram stringProgram = new StringProgram(userInput);
-                stringProgram.Print_First_Non_Repeating_Character().Wait();
-                stringProgram.Print_Lengthy_Word_from_given_Statement().Wait();
-                stringProgram.Print_Vowels_With_Count().Wait();
-                stringProgram.Print_Reverse_Words().Wait();
-                stringProgram.Print_Letters_Count().Wait();
-                #endregion
-
-                Console.ReadKey();
-                Console.Write("Do you want to continue? (y/n): ");
+                Console.Write("\nDo you want to continue? (y/n): ");
                 string continueInput = Console.ReadLine();
 
                 if (continueInput?.ToLower() != "y")
                 {
                     break;
                 }
-            }            
+            }
         }
     }
 }
---
+++ Preparation/Programs/MiscellaneousProgram.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs
+{
+    public interface IMiscellaneousProgram
+    {
+        Task Remove_Duplicate_elements_in_an_Array(int[] numbers);
+        Task Print_till_Sums_up_single_Digit(int number);
+        Task Print_Nth_Largest_Number(double[] numbers, int n_th_Index);
+        Task Print_Max_Negated_Number(double[] numbers);
+        Task Right_Shift_and_Print_Array_elements(int[] numbers, int times);
+    }
+
+    public class MiscellaneousProgram : IMiscellaneousProgram, IPrintService
+    {
+        #region Print Result
+        public Task PrintResult(object data, string message)
+        {
+            string result = "";
+            if (data is int[] numbers)
+            {
+                result = string.Join(", ", numbers);
+            }
+            else
+            {
+                result = data.ToString();
+            }
+            Console.WriteLine($"{message}{result}\n");
+            return Task.CompletedTask;
+        }
+        #endregion
+
+        public Task Print_Max_Negated_Number(double[] numbers)
+        {
+            if (numbers.Length > 1)
+            {
+                int foundNegatedNumberFlag = 0;
+                for (int maxNegatedNumberIndex = 0; maxNegatedNumberIndex < numbers.Length; maxNegatedNumberIndex++)
+                {
+                    if (numbers[maxNegatedNumberIndex] < 0)
+                    {
+                        foundNegatedNumberFlag = 1;
+                        double maxNegatedNumber = numbers[maxNegatedNumberIndex];
+                        if (maxNegatedNumberIndex != (numbers.Length - 1))
+                        {
+                            for (int currentIndex = maxNegatedNumberIndex + 1; currentIndex < numbers.Length; currentIndex++)
+                            {
+                                if ((numbers[currentIndex] < 0) && (maxNegatedNumber < numbers[currentIndex]))
+                                {
+                                    maxNegatedNumber = numbers[currentIndex];
+                                }
+                            }
+                        }
+                        PrintResult(
+                            maxNegatedNumber, 
+                            $"Max negated number in the given set [{string.Join(", ", numbers)}] is: "
+                        );
+                        break;
+                    }
+                }
+                if (foundNegatedNumberFlag == 0)
+                {
+                    Console.WriteLine($"Didn't find any negated numbers in the given set: [{string.Join(", ", numbers)}]");
+                }
+            }
+            else
+            {
+                Console.WriteLine("We didn't find any number set to perform the operation.");
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task Print_Nth_Largest_Number(double[] numbers, int n_th_Index)
+        {
+            if (numbers.Length > 1)
+            {
+                if ((n_th_Index >= 0) && (n_th_Index <= numbers.Length))
+                {
+                    double[] sortedNumbers = (from number in numbers orderby number descending select number).ToArray<double>();
+                    PrintResult(
+                        sortedNumbers[n_th_Index - 1],
+                        $"The {n_th_Index}th largest number in the given set [{string.Join(", ", numbers)}] is: "
+                    );
+                }
+                else
+                {
+                    Console.WriteLine($"The given index {n_th_Index} is out of range for the given set of numbers [{string.Join(", ", numbers)}]");
+                }
+            }
+            else
+            {
+                Console.WriteLine("We didn't find any number set to perform the operation.");
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task Print_till_Sums_up_single_Digit(int number)
+        {
+            if (number > 0)
+            {
+                int digitSum = 0;
+                while (number != 0)
+                {
+                    digitSum += Convert.ToInt16(number % 10);
+                    number = number / 10;
+                }
+
+                return digitSum.ToString().Length == 1 ? PrintResult(
+                                                            digitSum, 
+                                                            $"The single-digit sum for the given number is: "
+                                                         ) 
+                                                       : Print_till_Sums_up_single_Digit(digitSum);
+            }
+            else
+            {
+                Console.WriteLine("Please enter at least a double digit positive number");
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task Remove_Duplicate_elements_in_an_Array(int[] numbers)
+        {
+            if (numbers.Length > 1)
+            {
+                int[] distinctNumbers = numbers.Distinct().ToArray<int>();
+                PrintResult(
+                    distinctNumbers, 
+                    $"The distinct elements in the given set [{string.Join(", ", numbers)}] are: "
+                );
+            }
+            else
+            {
+                Console.WriteLine("We didn't find any number set to perform the operation.");
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task Right_Shift_and_Print_Array_elements(int[] numbers, int times)
+        {
+            if (numbers.Length > 1)
+            {
+                for (int step = 1; step <= times; step++)
+                {
+                    int number = numbers[0];
+                    for (int i = 1; i < numbers.Length; i++)
+                    {
+                        int temp = numbers[i];
+                        numbers[i] = number;
+                        number = temp;
+                    }
+                    numbers[0] = number;
+                }
+                PrintResult(
+                    numbers,
+                    $"The result after right-shifting elements {times} times is: "
+                );
+            }
+            else
+            {
+                Console.WriteLine("We didn't find any number set to perform the operation.");
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
